5beda3326b220471e9b92a9c0d09938898f185fa
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-25 16:45:05 +0800

    style(app_builder): in-consistent formatting

--- engine/misc/app_builder.hpp
+++ engine/misc/app_builder.hpp
@@ -12,63 +12,63 @@ private:
 public:
   explicit constexpr AppBuilder() = default;
 
-  [[nodiscard]] constexpr AppBuilder & loadConfig(std::string file_path) noexcept {
-    // TODO:
+  [[nodiscard]] constexpr AppBuilder& loadConfig(std::string file_path) noexcept {
+    /// TODO:
     (void)file_path;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder & setTitle(std::string new_title) noexcept {
+  [[nodiscard]] constexpr AppBuilder& setTitle(std::string new_title) noexcept {
     _app_config.title = std::move(new_title);
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder & setWindowDimension(util::Vec2i new_dimension) noexcept {
+  [[nodiscard]] constexpr AppBuilder& setWindowDimension(util::Vec2i new_dimension) noexcept {
     _app_config.window_size = new_dimension;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder & setWindowWidth(float new_value) noexcept {
+  [[nodiscard]] constexpr AppBuilder& setWindowWidth(float new_value) noexcept {
     _app_config.window_size.x = new_value;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder & setWindowHeight(float new_value) noexcept {
+  [[nodiscard]] constexpr AppBuilder& setWindowHeight(float new_value) noexcept {
     _app_config.window_size.y = new_value;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder & setTargetFPS(int new_fps) noexcept {
+  [[nodiscard]] constexpr AppBuilder& setTargetFPS(int new_fps) noexcept {
     _app_config.target_fps = new_fps;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder &maxTargetFPS() noexcept {
+  [[nodiscard]] constexpr AppBuilder& maxTargetFPS() noexcept {
     _app_config.target_fps = 0;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder &enableVsync() noexcept {
+  [[nodiscard]] constexpr AppBuilder& enableVsync() noexcept {
     _app_config.is_vsync = true;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder &enableWindowResizing() noexcept {
+  [[nodiscard]] constexpr AppBuilder& enableWindowResizing() noexcept {
     _app_config.is_resizeable = true;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder &disableVsync() noexcept {
+  [[nodiscard]] constexpr AppBuilder& disableVsync() noexcept {
     _app_config.is_vsync = false;
     return *this;
   }
 
-  [[nodiscard]] constexpr AppBuilder &disableWindowResizing() noexcept {
+  [[nodiscard]] constexpr AppBuilder& disableWindowResizing() noexcept {
     _app_config.is_resizeable = false;
     return *this;
   }
 
-  [[nodiscard]] core::App &build() const noexcept {
+  [[nodiscard]] core::App& build() const noexcept {
     core::App _ {_app_config};
     return core::App::instance();
   }
