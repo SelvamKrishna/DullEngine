4a25b9c7afa323b83aa625fcbe9e0ceb2b100723
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-11-09 13:01:28 +0800

    feat: safer casting of Event::_data

--- engine/core/event.hpp
+++ engine/core/event.hpp
@@ -2,6 +2,7 @@
 
 #include <any>
 #include <string>
+#include <optional>
 #include <functional>
 #include <unordered_map>
 
@@ -17,13 +18,24 @@ public:
 
   explicit Event(std::string name) noexcept : _name{std::move(name)} {}
 
+  [[nodiscard]] const std::string& getName() const noexcept { return _name; }
+
   template <typename T>
   [[nodiscard]] T getData(const std::string& key) const { return std::any_cast<T>(_data.at(key)); }
 
   template <typename T>
-  void setData(const std::string& key, T value) const { _data.at(key) = value; }
+  [[nodiscard]] std::optional<T> tryGetData(const std::string& key) const noexcept {
+    if (auto it = _data.find(key); it != _data.end()) {
+      if (T val = std::any_cast<T>(&it->second)) return *val;
+    }
 
-  [[nodiscard]] const std::string& getName() const noexcept { return _name; }
+    return std::nullopt;
+  }
+
+  template <typename T>
+  void setData(const std::string& key, T&& value) {
+    _data[key] = std::forward<T>(value);
+  }
 };
 
 using EventCallback = std::function<void(const Event&)>;
@@ -45,9 +57,9 @@ public:
     _listeners[event_name].push_back(std::move(callback));
   }
 
-  void emit(const Event& event) {
+  void emit(const Event& event) const noexcept {
     if (auto it = _listeners.find(event.getName()); it != _listeners.end()) {
-      for (auto& fn : it->second) fn(event);
+      for (const auto& FN : it->second) FN(event);
     }
   }
 };
