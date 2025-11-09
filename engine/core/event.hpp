bbf422918d660c2288084f30e741974629cf2b25
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-11-09 12:52:47 +0800

    feat: event bus

--- engine/core/event.hpp
+++ engine/core/event.hpp
@@ -1,22 +1,15 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
 #include <any>
+#include <string>
+#include <functional>
+#include <unordered_map>
 
 namespace dull::core {
-
-enum class EventCategory : uint8_t {
-  None,
-  Input,
-  Window,
-  Custom,
-};
 
 struct Event {
 private:
   std::string _name;
-  EventCategory _category {EventCategory::Custom};
   std::unordered_map<std::string, std::any> _data;
 
 public:
@@ -24,14 +17,39 @@ public:
 
   explicit Event(std::string name) noexcept : _name{std::move(name)} {}
 
-  explicit Event(std::string name, EventCategory category) noexcept
-  : _name{std::move(name)}, _category{category} {}
-
   template <typename T>
   [[nodiscard]] T getData(const std::string& key) const { return std::any_cast<T>(_data.at(key)); }
 
   template <typename T>
   void setData(const std::string& key, T value) const { _data.at(key) = value; }
+
+  [[nodiscard]] const std::string& getName() const noexcept { return _name; }
+};
+
+using EventCallback = std::function<void(const Event&)>;
+
+class EventBus {
+private:
+  std::unordered_map<std::string, std::vector<EventCallback>> _listeners;
+
+  explicit EventBus() = default;
+  ~EventBus() = default;
+
+  EventBus(const EventBus&) = default;
+  EventBus(EventBus&&) = default;
+  EventBus& operator=(const EventBus&) = default;
+  EventBus& operator=(EventBus&&) = default;
+
+public:
+  void subscribe(const std::string& event_name, EventCallback callback) {
+    _listeners[event_name].push_back(std::move(callback));
+  }
+
+  void emit(const Event& event) {
+    if (auto it = _listeners.find(event.getName()); it != _listeners.end()) {
+      for (auto& fn : it->second) fn(event);
+    }
+  }
 };
 
 } // namespace dull::core
