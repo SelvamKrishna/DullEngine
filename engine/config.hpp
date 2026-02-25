0f7ce7ea27eb82070a9b7623ba8c8e29086c1f83
SelvamKrishna <selvam.krishna0510@gmail.com>
2026-02-25 21:51:59 +0800

    Engine Version Tracking

--- engine/config.hpp
+++ engine/config.hpp
@@ -3,11 +3,19 @@
 #include <vendor/zutil/zutil.hpp>
 
 #include <cstdint>
+#include <format>
+#include <string>
 
 namespace dull::config {
 
-inline const zutil::ProString DULL_TAG = {"[DULL]", zutil::ANSI::EX_Black};
+inline constexpr uint8_t VERSION_MAJOR = 1;
+inline constexpr uint8_t VERSION_MINOR = 0;
+
+[[nodiscard]]
+inline std::string GetVersionString() noexcept { return std::format("{}.{}", VERSION_MAJOR, VERSION_MINOR); }
 
 inline constexpr uint32_t TICKS_PER_SECOND = 60;
+
+inline const zutil::ProString DULL_TAG = {"[DULL]", zutil::ANSI::EX_Black};
 
 } // namespace dull::config
